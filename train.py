c237385da4a1b5a2cdce7dc0d045b6186ff22d37
prarshah <52689055+prarshah@users.noreply.github.com>
2020-10-29 16:12:37 +0530

    Update train.py

--- train.py
+++ train.py
@@ -26,6 +26,11 @@ def eval_metrics(actual, pred):
 
 if __name__ == "__main__":
     mlflow.set_tracking_uri("http://localhost:5000")
+    mlflow.end_run()
+    mlflow.end_run()
+    mlflow.end_run()
+    mlflow.end_run()
+    mlflow.end_run()
     warnings.filterwarnings("ignore")
     np.random.seed(40)
 
